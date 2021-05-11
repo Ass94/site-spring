67a8d0ab0fe41397c7dc148493ef78a69061ede3
Alex <alex94tu@gmail.com>
2021-05-11 12:07:38 -0300

    Entidades Cliente e Endereço

---
+++ src/main/java/br/com/digite/model/Endereco.java
@@ -0,0 +1,109 @@
+package br.com.digite.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String cep;
+	private String logradouro;
+	private String numero;
+	private String Bairro;
+	private String cidade;
+	private String estado;
+	
+	public Endereco(Long id, String cep, String logradouro, String numero, String bairro, String cidade,
+			String estado) {
+		this.id = id;
+		this.cep = cep;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		Bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return Bairro;
+	}
+	public void setBairro(String bairro) {
+		Bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Endereco [id=" + id + ", cep=" + cep + ", logradouro=" + logradouro + ", numero=" + numero + ", Bairro="
+				+ Bairro + ", cidade=" + cidade + ", estado=" + estado + "]";
+	}
+	
+	
+	
+
+}
