13f12b8ad378a2a75140e444448316344a72f8dc
Alex <alex94tu@gmail.com>
2021-05-12 13:36:39 -0300

    Configuração do CrossOrigin

--- src/main/java/br/com/digite/resource/ClienteResource.java
+++ src/main/java/br/com/digite/resource/ClienteResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import br.com.digite.service.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
+@CrossOrigin("*")
 public class ClienteResource {
 	
 	@Autowired
