df3821a93e4f2354e5a5e4c45e671640653c7948
Alex <alex94tu@gmail.com>
2021-05-13 12:00:16 -0300

    Ajuste para enviar e-mail no ambiente de produção

--- src/main/java/br/com/digite/config/ProdConfig.java
+++ src/main/java/br/com/digite/config/ProdConfig.java
@@ -1,9 +1,14 @@
 package br.com.digite.config;
 
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.digite.service.DBService;
 import br.com.digite.service.EmailService;
 import br.com.digite.service.SmtpEmailService;
 
@@ -11,6 +16,22 @@ import br.com.digite.service.SmtpEmailService;
 @Profile("prod")
 public class ProdConfig {
 	
+	@Autowired
+	private DBService service;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean iniciar() throws ParseException{
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		service.iniciarBancoDados();
+		return true;
+	}
 	
 	@Bean
 	public EmailService emailService() {
